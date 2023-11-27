71c64f91cb3870ebf67f92ca53cee94a69d87f41
Keith Peters <kp@bit-101.com>
2023-11-26 20:18:19 -0500

    binary colorizors

--- colorizors/colorizors.go
+++ colorizors/colorizors.go
@@ -11,6 +11,41 @@ import (
 // Colorizor is a function that determines what color a point will be rendered with.
 type Colorizor func(m float64) blcolor.Color
 
+// Binary returns a ...
+func Binary(colA, colB blcolor.Color, iter int) Colorizor {
+	return func(m float64) blcolor.Color {
+		if int(m*float64(iter))%2 == 0 {
+			return colA
+		}
+		return colB
+	}
+}
+
+// BinaryZero returns a ...
+func BinaryZero(colA, colB, zero blcolor.Color, iter int) Colorizor {
+	return func(m float64) blcolor.Color {
+		if m == 0 {
+			return zero
+		}
+		if int(m*float64(iter))%2 == 0 {
+			return colA
+		}
+		return colB
+	}
+}
+
+// Duck returns a duck fractal colorizor.
+// x should be in the range of 2 up to ... anything. Higher numbers give smoother gradients.
+func Duck(r, g, b, x float64) Colorizor {
+	return func(m float64) blcolor.Color {
+		co := 1.0 - math.Log2(0.5*math.Log2(m*x))
+		red := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+r)
+		green := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+g)
+		blue := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+b)
+		return blcolor.RGB(red, green, blue)
+	}
+}
+
 // GreyScale is a simple greyscale colorizor
 func GreyScale(min, max float64) Colorizor {
 	return func(m float64) blcolor.Color {
@@ -34,17 +69,5 @@ func HSV(minHue, maxHue, minSat, maxSat, minVal, maxVal float64) Colorizor {
 		s := blmath.Lerp(m, minSat, maxSat)
 		v := blmath.Lerp(m, minVal, maxVal)
 		return blcolor.HSV(h, s, v)
-	}
-}
-
-// Duck returns a duck fractal colorizor.
-// x should be in the range of 2 up to ... anything. Higher numbers give smoother gradients.
-func Duck(r, g, b, x float64) Colorizor {
-	return func(m float64) blcolor.Color {
-		co := 1.0 - math.Log2(0.5*math.Log2(m*x))
-		red := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+r)
-		green := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+g)
-		blue := 0.5 + 0.5*math.Cos(blmath.TwoPi*co+b)
-		return blcolor.RGB(red, green, blue)
 	}
 }
